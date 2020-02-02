ebcf6a02a7078321a2073170af74d9290aedd2a5
Iury Vasconcelos <iury0393@gmail.com>
2020-02-02 14:01:10 -0300

    Ajustes finais

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,9 +18,14 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        //print(sender.currentTitle!)
-
+        
         playSound(soundName: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
     }
     
     func playSound(soundName: String) {
